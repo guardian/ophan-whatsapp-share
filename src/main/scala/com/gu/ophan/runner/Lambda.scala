9f64623b116d26d813434eeb3e6e82b8791b237b
Mariot Chauvin <mariot.chauvin@gmail.com>
2020-05-18 14:11:57 +0100

    Generate report on demand

--- src/main/scala/com/gu/ophan/runner/Lambda.scala
+++ src/main/scala/com/gu/ophan/runner/Lambda.scala
@@ -18,6 +18,8 @@ class Lambda {
     logger.log(s"first record: ${event.getRecords().get(0)}")
     logger.log(s"message: ${event.getRecords().get(0).getSNS().getMessage}")*/
 
+
+
     val today = DateTime.now.minusHours(2).withZone(DateTimeZone.forID("Europe/London"))
     val path = today.toString("yyyy-MM-dd-HH").toLowerCase
 
@@ -25,8 +27,28 @@ class Lambda {
     val json = ReportGenerator.generateReportForDate(today)
     S3.put(target, s"${path}.json", json)
 
+
     logger.log(s"Ending report at ${DateTime.now}")
-    
+
+    val month = System.getenv("MONTH")
+    val day = System.getenv("DAY")
+
+    if (month != null && day != null) {
+      val hours = List.range(0, 23)
+      for (h <- hours) {
+        val tz = DateTimeZone.forID("Europe/London")
+        val d =  new DateTime(2020, month.toInt, day.toInt, h, 0, tz)
+        logger.log(s"Generate report for ${today.toString("yyyy-MM-dd-HH")}")
+        gen(target, d)
+        logger.log(s"Ending report at ${DateTime.now}")
+      }
+    }
+  }
+
+  def gen(target: String, today: DateTime) {
+    val path = today.toString("yyyy-MM-dd-HH").toLowerCase
+    val json = ReportGenerator.generateReportForDate(today)
+    S3.put(target, s"${path}.json", json)
   }
 
 }
